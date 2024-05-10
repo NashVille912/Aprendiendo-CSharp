9c53c3f22a5ddb561db6b716ddba7b16b6ec5019
Nacho Villarraza <nachovillarraza@hotmail.com>
2024-05-10 08:06:57 -0300

    2 lecciones mas agregadas

---
+++ 10-ClasesAbstractas/Program.cs
@@ -0,0 +1,181 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Lagartija miLagartija = new Lagartija("Juancho");
+
+        miLagartija.respirar();
+        miLagartija.getNombre();
+    }
+}
+
+abstract class Animal
+{
+    public void respirar()
+    {
+        Console.WriteLine("Soy capaz de respirar");
+    }
+
+    public abstract void getNombre();
+}
+
+interface IMamiferosTerrestres
+{
+    int NumPatas();
+}
+
+interface IMamiferosEnDeportes
+{
+    string TipoDeporte();
+    bool EsOlimpico();
+}
+interface IMamiferosSaltador
+{
+    int NumPatas();
+}
+
+class Lagartija: Animal
+{
+    public Lagartija(string nombre)
+    {
+        this.nombre = nombre;
+    }
+    public override void getNombre()
+    {
+        Console.WriteLine(nombre);
+    }
+    private string nombre;
+}
+
+class Mamiferos: Animal
+{
+    public Mamiferos(string nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public void cuidarCrias()
+    {
+        Console.WriteLine("Cuido mi cria hata que se pueda valer por si sola");
+    }
+
+    public override void getNombre()
+    {
+        Console.WriteLine(nombre);
+    }
+
+    public virtual void pensar()
+    {
+        Console.WriteLine("Cada Mamifero puede pensar y tomar decisiones.");
+    }
+
+    string nombre;
+}
+
+class Caballo : Mamiferos, IMamiferosTerrestres, IMamiferosEnDeportes, IMamiferosSaltador
+{
+
+    public Caballo(string nombreCaballo) : base(nombreCaballo)
+    {
+
+    }
+
+    public void galopar()
+    {
+        Console.WriteLine("Soy capaz de galopar");
+    }
+
+    int IMamiferosTerrestres.NumPatas()
+    {
+        return 4;
+    }
+    int IMamiferosSaltador.NumPatas()
+    {
+        return 2;
+    }
+    public string TipoDeporte()
+    {
+        return "Hipica";
+    }
+    public bool EsOlimpico()
+    {
+        return true;
+    }
+}
+
+class Humano : Mamiferos, IMamiferosEnDeportes
+{
+
+    public Humano(string nombreHumano) : base(nombreHumano)
+    {
+
+    }
+    public sealed override  void pensar()
+    // Al usar la palabra NEW le estamos confirmando al compilador que nuestra idea era sobreescribir el metodoo.
+    {
+        Console.WriteLine("Soy capaz de pensar(?)");
+    }
+
+    public string TipoDeporte()
+    {
+        return "Futbol";
+    }
+    public bool EsOlimpico()
+    {
+        return true;
+    }
+
+}
+
+class Adolescente : Humano
+{
+    public Adolescente(string nombre) : base(nombre)
+    {
+
+    }
+
+    public override void pensar()
+    {
+        Console.WriteLine("Las hormonas me impiden pensar con claridad.");
+    }
+}
+
+class Gorila : Mamiferos
+{
+    public Gorila(string nombreGorila) : base(nombreGorila)
+    {
+
+    }
+    public override void pensar()
+    {
+        Console.WriteLine("Pensamiento Avanzado");
+    }
+
+    public void trepar()
+    {
+        Console.WriteLine("Soy capaz de trepar");
+    }
+
+}
+
+class Ballena : Mamiferos
+{
+    public Ballena(string nombre) : base(nombre)
+    {
+
+    }
+
+    public void nadar()
+    {
+        Console.WriteLine("Estoy nadando por los mares!");
+    }
+}
+
+class Chimpance: Gorila
+{
+    public Chimpance(string nombre): base(nombre)
+    {
+
+    }
+}
+
---
+++ 11-Propiedades/Program.cs
@@ -0,0 +1,108 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        //Propiedades
+
+
+        //Empleado juan = new Empleado("Juan");
+        //juan.SALARIO = 1200;
+        //Console.WriteLine($"El salario del empleado es de: {juan.SALARIO}");
+        //double bono = (juan.SALARIO * 30) / 100;
+        //Console.WriteLine($"Este mes el empleado con el bono del 30% cobrara: {juan.SALARIO + bono}");
+
+        //Structs
+
+
+        //Employee empleado = new Employee(1200, 250);
+        //Console.WriteLine(empleado.ToString());
+        //empleado.aumentarSalario(empleado, 250);
+        //Console.WriteLine(empleado.ToString());
+    
+        //Enums
+    }
+}
+
+enum Estaciones
+{
+    Verano, Otono,Invierno, Primavera
+}
+
+class Empleado {
+
+    public Empleado(string nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    //public void setSalario(double salario)
+    //{
+    //    if (salario < 0)
+    //    {
+    //        Console.WriteLine("El salario no puede ser negativo, se asignara por defecto 0");
+    //        this.salario = 0;
+    //    }
+    //    else
+    //    {
+    //        this.salario = salario;
+    //    }
+    //}
+
+
+    //public double getSalario()
+    //{
+    //    return salario;
+    //}
+
+
+    private double evaluaSalario(double salario)
+    {
+        if (salario < 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return salario;
+        }
+    }
+
+    public double SALARIO
+    {
+        //get { return salario; }
+        //set { this.salario = evaluaSalario(value);}
+
+        get => this.salario;
+        set => this.salario = evaluaSalario (value);
+    }
+
+
+    private string nombre;
+    private double salario;
+
+}
+struct Employee
+{
+
+    public Employee(double salario, double comision)
+    {
+        this.salario = salario;
+        this.comision = comision;
+    }
+
+    public override string ToString()
+    {
+        return $"Salario y comision del empleado {this.salario}, {this.comision}";
+    }
+
+    public void aumentarSalario(Employee empleado, double incremento)
+    {
+        empleado.salario += incremento;
+        Console.WriteLine($"El nuevo salario del empleado sera de ${empleado.salario}");
+    }
+
+
+    private double salario;
+    private double comision;
+
+}
